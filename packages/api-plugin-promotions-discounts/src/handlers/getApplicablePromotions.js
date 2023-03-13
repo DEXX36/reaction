80a535ac8204a76926d13b11df5ccaaf68aaa923
vanpho93 <vanpho02@gmail.com>
2023-03-13 10:45:46 +0700

    feat: update discountCalculationMethodOrder position
    Signed-off-by: vanpho93 <vanpho02@gmail.com>

--- packages/api-plugin-promotions-discounts/src/handlers/getApplicablePromotions.js
+++ packages/api-plugin-promotions-discounts/src/handlers/getApplicablePromotions.js
@@ -2,7 +2,6 @@ import _ from "lodash";
 import getPromotionCombinations from "./getPromotionCombinations.js";
 import getHighestCombination from "./getHighestCombination.js";
 
-const discountCalculationMethodOrder = ["flat", "percentage", "fixed", "none"];
 
 /**
  * @summary get all applicable promotions
@@ -10,11 +9,12 @@ const discountCalculationMethodOrder = ["flat", "percentage", "fixed", "none"];
  * @param {*} cart - The cart to apply the promotion to
  * @param {*} promotions - The promotions to apply
  * @returns {Promise<Array<Object>>} - An array of promotions
- */
+*/
 export default async function getApplicablePromotions(context, cart, promotions) {
   const promotionsWithoutShippingDiscount = _.filter(promotions, (promotion) => promotion.promotionType !== "shipping-discount");
   const shippingPromotions = _.differenceBy(promotions, promotionsWithoutShippingDiscount, "_id");
 
+  const discountCalculationMethodOrder = ["flat", "percentage", "fixed", "none"];
   const sortedPromotions = _.sortBy(promotionsWithoutShippingDiscount, (promotion) => {
     const method = promotion.actions[0]?.actionParameters?.discountCalculationMethod || "none";
     return discountCalculationMethodOrder.indexOf(method);
